5229c4c29d64a31f402e6673726a36f934d55d78
ltb1996 <610030835@qq.com>
2025-03-19 09:28:50 +0800

    changejs

--- js/app1.js
+++ js/app1.js
@@ -1,44 +1,38 @@
-// 使用let 声明变量
-let tasks = [];
-let currentFilter = "all";
-
-// 使用const声明变量
-const taskList = document.getElementById("taskList");
-const addBtn = document.getElementById("addBtn");
-const taskInput = document.getElementById("taskInput");
-const statusElement = document.getElementById("stats");
-
-// 使用数值类型示例
-const defaultTaskCount = 0;
-const defaultCompletionRate = 0;
-
-const appName = "任务管理系统";
-const appVersion = "1.0.0";
-const appDescription = `${appName} 版本${appVersion}。`;
-console.log(appDescription);
-
-const isAppRunning = true;
-const isDebugMode = true;
-//添加任务函数
-function addTask() {
-    const currentTaskCount = tasks.length + 1;
-    if (taskInput.value === "") {
-        alert("任务不能为空");
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+let tasks = [
+    {
+        text:"ffff4", createdAt: new Date(2025, 1, 14),
+    },{
+        
+        text:"7767764", createdAt: new Date(2025, 1, 16),
     }
-
-    // if (){}
-
-    if 
-
-    const newTask = {
-        id: Date.now(),
-        text: taskInput.value,
-        completed: false,
-        createAt: new Date(),
-    }
-
-    tasks.push(newTask);
-
-    taskInput.value = "";
-
+];
+function update() {
+    taskList.innerHTML = '';
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.className = 'task-text';
+        span.textContent = task.text;
+        li.appendChild(span);
+        taskList.appendChild(li);
+    })
+ 
 }
+function addTask() {
+   if(!taskInput.value) {
+    alert('Please enter a task');
+    return;
+   }
+   const newtask = {
+    text: taskInput.value,
+    createdAt: new Date(),
+   };
+   tasks.push(newtask);
+   taskInput.value = '';
+   update()
+}
+addBtn.addEventListener('click', addTask);
+update()
