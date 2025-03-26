a1bd5aaacd918de20174a93a62ca3ae2f690bd88
ltb1996 <610030835@qq.com>
2025-03-26 08:07:55 +0800

    js

--- js/app1.js
+++ js/app1.js
@@ -1,9 +1,12 @@
 const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
+const statusElement = document.getElementById('status');
+const sortByNameBtn = document.getElementById('sortByName');
+const sortByDateBtn = document.getElementById('sortByDate');
 let tasks = [
     {
-        text:"ffff4", createdAt: new Date(2025, 1, 14),
+        text:"ffff4", createdAt: new Date(2025, 2, 14),
     },{
         
         text:"7767764", createdAt: new Date(2025, 1, 16),
@@ -16,10 +19,14 @@ function update() {
         const span = document.createElement('span');
         span.className = 'task-text';
         span.textContent = task.text;
+        const dateSpan = document.createElement('span');
+        dateSpan.className = 'task-date';
+        dateSpan.textContent = "("+ task.createdAt.toLocaleDateString() + ")";
         li.appendChild(span);
+        li.appendChild(dateSpan);
         taskList.appendChild(li);
     })
- 
+    statusElement.innerHTML = `当前总任务数： ${tasks.length} `;
 }
 function addTask() {
    if(!taskInput.value) {
@@ -35,4 +42,20 @@ function addTask() {
    update()
 }
 addBtn.addEventListener('click', addTask);
-update()
+taskInput.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+        addTask();
+    }
+})
+function sortTasksByName() {
+    tasks.sort((a, b) => a.text.localeCompare(b.text));
+    update();
+}
+function sortTasksByDate() {
+    tasks.sort((a, b) => a.createdAt - b.createdAt);
+    update();
+}
+
+sortByNameBtn.addEventListener('click', sortTasksByName);
+sortByDateBtn.addEventListener('click', sortTasksByDate);
+update();
